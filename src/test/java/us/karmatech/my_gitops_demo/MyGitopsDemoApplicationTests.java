e61333289cd84a510388237d52b83f7d60cecf39
iqbal asus linux <iqbalyusufdipu@gmail.com>
2025-02-28 16:07:36 +0600

    Start GitOps with Java, Spring Boot, Microk8s, ArgoCD

---
+++ src/test/java/us/karmatech/my_gitops_demo/MyGitopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package us.karmatech.my_gitops_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGitopsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
