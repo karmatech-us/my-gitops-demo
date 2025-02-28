b613606bc4c5ac5b2f21ff66911c83e416294394
iqbal asus linux <iqbalyusufdipu@gmail.com>
2025-02-28 22:48:18 +0600

    Test ArgoCD

--- src/main/java/us/karmatech/my_gitops_demo/MyGitopsDemoApplication.java
+++ src/main/java/us/karmatech/my_gitops_demo/MyGitopsDemoApplication.java
@@ -11,7 +11,7 @@ public class MyGitopsDemoApplication {
 
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello, GitOps..............";
+		return "Hello, ArgoCD......";
 	}
 
 	public static void main(String[] args) {
